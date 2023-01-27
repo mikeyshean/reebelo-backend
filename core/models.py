98e4ad9d922c4b14094a4e6d99881053b8eaca19
Mikey Shean <mdshean2@gmail.com>
2023-01-27 14:34:09 -0800

    restructure project

--- core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class TimestampedModel(models.Model):
-    created = models.DateTimeField(default=timezone.now)
-    modified = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
